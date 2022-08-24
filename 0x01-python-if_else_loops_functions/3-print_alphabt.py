f6dac6b77e154559e20a8905493e86afbfc9fa33
shady-cj <petersp2000@gmail.com>
2022-08-24 01:47:28 +0100

    feat+docs: print alternating case alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for alpha in range(ord('a'), ord('z')):
+for alpha in range(ord('a'), ord('z') + 1):
     if chr(alpha) == 'e' or chr(alpha) == 'q':
         continue
     print("{}".format(chr(alpha)), end="")
