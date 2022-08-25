a8ba1990459114e7603116fddfae3cbef93e69cb
shady-cj <petersp2000@gmail.com>
2022-08-25 19:42:52 +0100

    feat+docs: A program that performs basic arithmetic operations from the command line

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    if len(sys.argv) == 1:
+    if len(sys.argv) == 1 or len(sys.argv) > 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
