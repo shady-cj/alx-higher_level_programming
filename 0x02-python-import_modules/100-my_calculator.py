c460363d20b97dbb9e5b685f89c99f14fd0d711f
shady-cj <petersp2000@gmail.com>
2022-08-25 19:55:19 +0100

    feat+docs: A program that performs basic arithmetic operations from the command line

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
     if len(sys.argv) == 1 or len(sys.argv) > 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
@@ -22,7 +22,6 @@ if __name__ == "__main__":
         case _:
             message = "Unknown operator. Available operators: +, -, * and /"
     if result is None:
-        print(message)
+        print("{}".format(message))
         sys.exit(1)
     print("{} {} {} = {}".format(a, op, b, result))
-
