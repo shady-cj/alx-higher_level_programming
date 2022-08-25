0f773ea04b50520724d3b413e90e42f3a023ea9c
shady-cj <petersp2000@gmail.com>
2022-08-25 23:11:55 +0100

    feat+docs: A program that reverses bytecode to the original python code

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-import magic_calculation_102
+from magic_calculation_102 import add, sub
 def magic_calculation(a, b):
-    add = magic_calculation_102.add
-    sub = magic_calculation_102.sub
-
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
