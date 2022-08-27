d1fda2bf690dcaa82216473154875173f1385d65
shady-cj <petersp2000@gmail.com>
2022-08-27 19:02:46 +0100

    feat+docs: A program that prints a list in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if (my_list == None):
+        return None
     i = len(my_list) - 1
     while (i >= 0):
         print("{:d}".format(my_list[i]))
