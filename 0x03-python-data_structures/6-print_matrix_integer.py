8bf3923b664d161a9c5e11cbe2f437c82b174ba8
shady-cj <petersp2000@gmail.com>
2022-08-27 19:58:22 +0100

    feat+docs: A program that modifies that provides a function that prints a matrix of integers.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
     for array in matrix:
         for j, i in enumerate(array):
             endstring = "" if j == (len(array) - 1) else " "
-            print("{}".format(i), end=endstring)
+            print("{:d}".format(i), end=endstring)
         print("")
