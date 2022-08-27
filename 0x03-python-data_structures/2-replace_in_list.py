601ca5217bd5e3ebd5a16b89d115fb04fa876566
shady-cj <petersp2000@gmail.com>
2022-08-27 18:38:02 +0100

    feat+docs: A program that changes an element in a list and returns the new list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
-#!/ust/bin/python3
+#!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if idx > 0 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
     return my_list
