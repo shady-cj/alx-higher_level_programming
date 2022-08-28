37bc22e2543854dce21a5a9fb3ed054f2711e34c
shady-cj <petersp2000@gmail.com>
2022-08-28 12:39:44 +0100

    feat+docs: A program that returns a new list based on if the entries of a given list are even or odd

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,9 +3,9 @@
 def divisible_by_2(my_list=[]):
     is_divisible = []
 
-    for l in my_list:
-        if l % 2 == 0:
+    for entry in my_list:
+        if entry % 2 == 0:
             is_divisible.append(True)
         else:
             is_divisible.append(False)
-    return is_divisible 
+    return is_divisible
