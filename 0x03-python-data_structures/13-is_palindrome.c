acc55f3826a5bd63513025729e31c5987768c70d
shady-cj <petersp2000@gmail.com>
2022-08-29 09:52:30 +0100

    feat+docs: A C program that checks if a linked list is palindromic (optimized version)

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -13,6 +14,7 @@ int is_palindrome(listint_t **head)
 	listint_t *new_head = NULL;
 	listint_t *ptr_1 = NULL;
 	listint_t *ptr_2 = NULL;
+	int is_palindrome;
 
 	if (*head == NULL)
 		return (1);
@@ -20,18 +22,9 @@ int is_palindrome(listint_t **head)
 	ptr_1 = *head;
 	ptr_2 = new_head;
 
-	while (ptr_1 != NULL && ptr_2 != NULL)
-	{
-		if (ptr_1->n != ptr_2->n)
-			return (0);
-		ptr_1 = ptr_1->next;
-		ptr_2 = ptr_2->next;
-	}
+	is_palindrome = compare_lists(ptr_1, ptr_2);
 	free_listint(new_head);
-	if (ptr_1 == NULL && ptr_2 == NULL)
-		return (1);
-	else
-		return (0);
+	return (is_palindrome);
 }
 
 /**
@@ -65,4 +58,21 @@ void duplicate_linked_list(listint_t **new_head, listint_t *head)
 		current = current->next;
 	}
 	*new_head = ptr;
+}
+
+/**
+ * compare_lists - This function compares the 2 lists.. the main list and
+ * the reversed list recursively
+ * @reversed_head: The head node to the reversed list
+ * @head: The head node to the main list
+ * Return: 1 if palindromic and 0 if not
+ */
+
+int compare_lists(listint_t *head, listint_t *reversed_head)
+{
+	if (head == NULL || reversed_head == NULL)
+		return (1);
+	if (head->n != reversed_head->n)
+		return (0);
+	return (compare_lists(head->next, reversed_head->next));
 }
