2654dbb061c8d3c48c94c056ef73f0bc40df27df
shady-cj <petersp2000@gmail.com>
2022-09-12 18:13:44 +0100

    feat+docs:  a function that divides 2 integers and prints the result.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,5 +8,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         pass
     finally:
-        print("Inside result:", result)
+        print("Inside result: {}".format(result))
     return result
