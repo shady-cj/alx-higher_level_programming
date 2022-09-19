7cb0c5b3542e1c13b257a18ea62f19e994ee3f68
shady-cj <petersp2000@gmail.com>
2022-09-19 19:03:55 +0100

    feat+docsDefining a Rectangle class that calculates the perimeter and area

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,6 +49,7 @@ class Rectangle:
     def area(self):
         """ Returns the area """
         return self.width * self.height
+
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
