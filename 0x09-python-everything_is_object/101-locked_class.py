77e96768bc6c66e8a8f736da73d9ea3598bc2d44
shady-cj <petersp2000@gmail.com>
2022-09-20 20:56:49 +0100

    answer to Task number 31

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 
 """
-Defining a class that overwrites the setattr() method
+Defining a class that restricts dynamically allocating memory
 """
 
 
 class LockedClass:
-    """ Locked class overwrites the setattr() method and only allows
-    setting new attribute if the name is "first_name"
+    """ Locked class deines a __slot__ to limit the attributes that
+    can be stored at every instance call
     """
-    def __setattr__(self, name, value):
-        if name == "first_name":
-            self.__dict__[name] = value
-        else:
-            raise AttributeError("'" + str(type(self).__name__) + "'"
-                                 + " object has no attribute "
-                                 + "'" + str(name) + "'")
+    __slots__ = ["first_name"]
