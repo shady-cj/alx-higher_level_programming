d646802c2aa54ac5f905075fb95582f74822a11f
shady-cj <petersp2000@gmail.com>
2022-09-20 13:52:46 +0100

    bugs+docs: Fixed errors in the matrix_mul function and also test the function against edge cases

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,38 +1,61 @@
-
 #!/usr/bin/python3
 
 
+"""
+A module that provides a function that multiply two matrix.
+"""
+
 
 def matrix_mul(m_a, m_b):
+    """
+    The function multiplies 2 matrix (list of lists)
+    and raises exception if there is any form of
+    impossibilities in multiplying
+    """
     if type(m_a) != list:
         raise TypeError("m_a must be a list")
     if type(m_b) != list:
         raise TypeError("m_b must be a list")
-    if len(m_a) == 0:
-        raise ValueError("m_a can't be empty")
-    if len(m_b) == 0:
-        raise ValueError("m_b can't be empty")
 
     result = []
-    len_a = len(m_a[0])
-    len_b = len(m_b[0])
+    len_a = len(m_a[0]) if len(m_a) and type(m_a[0]) == list else 0
+    len_b = len(m_b[0]) if len(m_b) and type(m_b[0]) == list else 0
 
-    for a in range(len(m_a)):
-        new_arr = []
-        row = m_a[a]
-        col = m_b[a]
-        if len(row) != len_a:
+    for ma in m_a:
+        if type(ma) != list:
+            raise TypeError("m_a must be a list of lists")
+        if len(ma) != len_a:
             raise TypeError("each row of m_a must be of the same size")
-        if len(col) != len_b:
+        for a in ma:
+            if type(a) not in (int, float):
+                raise TypeError("m_a should contain only integers or floats")
+    for mb in m_b:
+        if type(mb) != list:
+            raise TypeError("m_b must be a list of lists")
+        if len(mb) != len_b:
             raise TypeError("each row of m_b must be of the same size")
-        for i in range(len(col)):
+        for b in mb:
+            if type(b) not in (int, float):
+                raise TypeError("m_b should contain only integers or floats")
+
+    if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
+        raise ValueError("m_a can't be empty")
+    if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
+        raise ValueError("m_b can't be empty")
+
+    for idx in range(len(m_a)):
+        new_arr = []
+        row = m_a[idx]
+        for i in range(len_b):
             points = 0
             for j in range(len(row)):
-                if type(row[j]) not in (int, float):
-                    raise TypeError("m_a should contain only integers or floats")
-                if type(m_b[j][i]) not in (int, float):
-                    raise TypeError("m_b should contain only integers or floats")
-                points += row[j] * m_b[j][i]
+                try:
+                    points += row[j] * m_b[j][i]
+                except IndexError:
+                    raise ValueError("m_a and m_b \
+can't be multiplied") from None
             new_arr.append(points)
         result.append(new_arr)
+    if len_a != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
     return result
--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import numpy as np
 
+"""
+Provides a function that multiplies two matrix
+"""
+
 
 def lazy_matrix_mul(m_a, m_b):
+    """ Multiplies two matrix using numpy module """
     return np.matmul(m_a, m_b)
