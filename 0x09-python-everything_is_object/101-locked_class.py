2ed5cf9711d4dedb81b15573d876e6d006ce885f
shady-cj <petersp2000@gmail.com>
2022-09-20 19:57:25 +0100

    implementing the Locked Class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -11,8 +11,7 @@ class LockedClass:
     """
     def __setattr__(self, name, value):
         if name == "first_name":
-            # self.__dict__[name] = value
-            super().__setattr__(name, value)
-            return None
-        raise AttributeError(f"'{type(self).__name__}' object has \
+            self.__dict__[name] = value
+        else:
+            raise AttributeError(f"'{type(self).__name__}' object has \
 no attribute '{name}'")
