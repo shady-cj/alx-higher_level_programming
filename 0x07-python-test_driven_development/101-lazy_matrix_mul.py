03096214cf86ad3d33369b3ee4aeeef8f4d30104
shady-cj <petersp2000@gmail.com>
2022-09-21 09:24:08 +0100

    updating 101-lazy_matrix_mul.py test cases

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,7 +7,7 @@ This module Provides a function that multiplies two matrix
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """ 
-    Multiplies two matrix using numpy module 
+    """
+    Multiplies two matrix using numpy module
     """
     return np.matmul(m_a, m_b)
