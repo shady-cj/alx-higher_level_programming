76608528c536bcf5468bebbdc9743d3111533f1d
shady-cj <petersp2000@gmail.com>
2022-09-21 08:00:37 +0100

    Added new edge cases to the unittest in 6-max_integer.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -30,3 +30,18 @@ class TestMaxInteger(unittest.TestCase):
         """
         self.assertEqual(max_integer([0, 0, 0, 0]), 0)
         self.assertEqual(max_integer([5, 2, 3, 5]), 5)
+
+    def test_one_element_in_the_list(self):
+        """
+        Test for one element in the list
+        """
+        self.assertEqual(max_integer([8]), 8)
+        self.assertIsNotNone(max_integer([0]))
+    
+    def test_for_maxelement_in_the_middle(self):
+        """
+        Test when max element is in the middle
+        """
+
+        self.assertEqual(max_integer([3,2,6,1,9,5,7,1,4]), 9)
+        self.assertEqual(max_integer([2,4,4,1]), 4)
