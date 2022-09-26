a191c20d514fd5a650394fdc31a246f709c651cd
Peter <petersp2000@gmail.com>
2022-09-26 14:55:42 -0700

    pycodsytle

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,9 +3,12 @@
 implementing myint that inherits from int
 """
 
+
 class MyInt(int):
     """Myint inherits from int and inverts == and !="""
+
     def __eq__(self, other):
         return self.real != other
+
     def __ne__(self, other):
         return self.real == other
