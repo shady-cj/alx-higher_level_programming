06c5d673251afe10b51b599df9b9a68940d6cccd
Peter <petersp2000@gmail.com>
2022-09-26 11:54:13 -0700

    Implementing printing all attributes of class

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+"""
+This module returns the attributes and methods of a class
+"""
+
+def lookup(obj):
+    """
+    Returns the list of the methods and attributes
+    """
+    return (list(dir(obj)))
