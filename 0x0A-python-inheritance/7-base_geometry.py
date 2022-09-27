9b71b7617b84f0f4976e2567f64c4ccc3d16a224
Peter <petersp2000@gmail.com>
2022-09-27 02:57:31 -0700

    correction

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -22,5 +22,5 @@ class BaseGeometry:
         except (TypeError, ValueError):
             raise TypeError(f"{name} must be an integer") from None
         if num <= 0:
-            raise ValueError(f"{name} must be greater than")
+            raise ValueError(f"{name} must be greater than 0")
         return value
