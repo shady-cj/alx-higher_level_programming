b9082386df327847fb98da5bf4dd9f3a1269a6bb
shady-cj <petersp2000@gmail.com>
2022-09-28 14:06:47 +0100

    pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-
-
 """
 This module provides a function that reads command
 line argument adds them to a list and serialize into
 a file
 """
+import json
+import sys
 load = __import__("6-load_from_json_file").load_from_json_file
 save = __import__("5-save_to_json_file").save_to_json_file
-import json
-
 
 
 def add_item(args, filename):
     try:
         list_obj = load(filename)
-    #except json.decoder.JSONDecodeError:
     except FileNotFoundError:
         list_obj = []
     i = 1
@@ -26,5 +23,4 @@ def add_item(args, filename):
 
 
 if __name__ == "__main__":
-    import sys
     add_item(sys.argv, "add_item.json")
