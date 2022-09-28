fab2b9e63038966c006acae5e558405f248000c5
shady-cj <petersp2000@gmail.com>
2022-09-28 11:15:32 +0100

    adding docstrings

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,6 @@ This module contains a function that reads a file
 
 
 def read_file(filename=""):
+    """ reads the file with the name filename and prints it out """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
