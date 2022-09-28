de69f6d6b946cffac536f2c83dc6edb95e5c04f9
shady-cj <petersp2000@gmail.com>
2022-09-28 23:36:07 +0100

    pycodestyle

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -16,19 +16,19 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs != None and type(attrs) == list:
+        if attrs is not None and type(attrs) == list:
             new_dict = {}
             for el in attrs:
                 dict_val = self.__dict__.get(el)
-                if dict_val != None:
+                if dict_val is not None:
                     new_dict[el] = dict_val
             return new_dict
         return self.__dict__
 
     def reload_from_json(self, json):
-        d = dict(self.__dict__)
+        d = self.__dict__
         for k, v in json.items():
             dict_value = d.get(k)
-            if dict_value != None:
+            if dict_value is not None:
                 d[k] = v
         return d
