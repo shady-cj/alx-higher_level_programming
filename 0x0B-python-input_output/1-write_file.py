f2dffb7f2a62058858802a0c49f9b70f35dfc5f0
shady-cj <petersp2000@gmail.com>
2022-09-28 11:29:00 +0100

    docstring

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,6 +8,9 @@ to a file.
 
 
 def write_file(filename="", text=""):
+    """
+    Writing into filename that value ot text
+    """
     num_of_char = 0
     with open(filename, "w", encoding="utf-8") as f:
         num_of_char = f.write(str(text))
