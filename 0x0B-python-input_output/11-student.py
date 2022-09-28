76e3a2486a20a7eb7293b0fe738246c13ca2fe48
shady-cj <petersp2000@gmail.com>
2022-09-28 23:27:54 +0100

    improved on number 10

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+A module that defines the student class
+"""
+
+
+class Student:
+    """
+    Defining public attributes and converting it to json
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs != None and type(attrs) == list:
+            new_dict = {}
+            for el in attrs:
+                dict_val = self.__dict__.get(el)
+                if dict_val != None:
+                    new_dict[el] = dict_val
+            return new_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        d = dict(self.__dict__)
+        for k, v in json.items():
+            dict_value = d.get(k)
+            if dict_value != None:
+                d[k] = v
+        return d
