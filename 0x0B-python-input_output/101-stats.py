1a5ab3be4f6701dfccb4fbcede7fa30bfbd660eb
shady-cj <petersp2000@gmail.com>
2022-09-29 06:10:01 +0100

    implementing stats

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -20,19 +20,26 @@ def stats():
     i = 0
     stats = {}
     file_size = 0
-    for line in sys.stdin:
-        if i == 10:
-            print(f"File size: {file_size}")
-            for s in stat_codes:
-                if stats.get(s) is not None:
-                    print(f"{s}: {stats[s]:d}")
-            i = 0
-            continue
-        split = line.split()
-        file_size += int(split[-1])
-        code = int(split[-2])
-        stats[code] = stats.get(code, 0) + 1
-        i += 1
+    try:
+
+        for line in sys.stdin:
+            if i == 10:
+                print(f"File size: {file_size}")
+                for s in stat_codes:
+                    if stats.get(s) is not None:
+                        print(f"{s}: {stats[s]:d}")
+                i = 0
+                continue
+            split = line.split()
+            file_size += int(split[-1])
+            code = int(split[-2])
+            stats[code] = stats.get(code, 0) + 1
+            i += 1
+    except KeyboardInterrupt:
+        print(f"File size: {file_size}")
+        for s in stat_codes:
+            if stats.get(s) is not None:
+                print(f"{s}: {stats[s]:d}")
 
 
 if __name__ == "__main__":
