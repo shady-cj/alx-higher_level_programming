d32cb68533a03db5afe494bc02d152780dc3411f
shady-cj <petersp2000@gmail.com>
2022-09-29 05:47:11 +0100

    implementing stats

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This module prints stats from standard input
+"""
+
+import sys
+
+
+def stats():
+    stat_codes = (
+            200,
+            301,
+            400,
+            401,
+            403,
+            404,
+            405,
+            500
+            )
+    i = 0
+    stats = {}
+    file_size = 0
+    for line in sys.stdin:
+        if i == 10:
+            print(f"File Size: {file_size}")
+            for s in stat_codes:
+                if stats.get(s) is not None:
+                    print(f"{s}: {stats[s]}\n")
+            file_size = 0
+            stats = {}
+            i = 0
+            continue
+        split = line.split()
+        file_size += int(split[-1])
+        code = int(split[-2])
+        stats[code] = stats.get(code, 0) + 1
+        i += 1
+
+if __name__ == "__main__":
+    stats()
