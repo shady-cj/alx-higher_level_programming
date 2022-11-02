3086c909cc912ab879a5c6518bdf5520eb7aa03f
Peter <petersp2000@gmail.com>
2022-11-02 12:09:15 -0700

    updating utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
 -- Converting a database to utf8(utf8mb4, collate utf8mb4_unicode_ci)
 
 
-ALTER DATABASE htbn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
