c19587825e95e778a2661ae64fedfc218df5df64
shady-cj <petersp2000@gmail.com>
2022-11-03 22:58:51 +0100

    script that shows the genres in the database and the number of shows linked

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
 -- a script that shows the genres in the database and the number of shows linked
 
 
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
