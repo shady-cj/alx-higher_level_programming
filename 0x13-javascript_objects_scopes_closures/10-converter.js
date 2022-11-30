e527f17f5defe86ce584a0cc804046a15e222757
peter <petersp2000@gmail.com>
2022-11-30 17:21:15 +0100

    challenge 10 implementation

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,24 +2,24 @@
 
 exports.converter = function (base) {
   return function (number) {
-    let conversion = '';
-    let quotient = number;
-    const hexes = {
-      10: 'A',
-      11: 'B',
-      12: 'C',
-      13: 'D',
-      14: 'E',
-      15: 'F'
-    };
-    while (quotient > 0) {
-      let rem = quotient % base;
-      if (rem > 9) {
-        rem = hexes[rem];
-      }
-      conversion = rem.toString() + conversion;
-      quotient = Math.floor(quotient / base);
-    }
-    return conversion;
+    // let conversion = '';
+    // let quotient = number;
+    // const hexes = {
+    //   10: 'A',
+    //   11: 'B',
+    //   12: 'C',
+    //   13: 'D',
+    //   14: 'E',
+    //   15: 'F'
+    // };
+    // while (quotient > 0) {
+    //   let rem = quotient % base;
+    //   if (rem > 9) {
+    //     rem = hexes[rem];
+    //   }
+    //   conversion = rem.toString() + conversion;
+    //   quotient = Math.floor(quotient / base);
+    // }
+    return number.toString(base);
   };
 };
