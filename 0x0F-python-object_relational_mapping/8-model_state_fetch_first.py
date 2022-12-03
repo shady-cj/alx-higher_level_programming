886c2f16eeca6d548a14dee5740e20bdbc1b3b73
peter <petersp2000@gmail.com>
2022-12-03 19:21:45 +0100

    Challenge #8 solution

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Issueing a query to fetch the first data from a given database
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'\
+            .format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    session = Session(bind=engine)
+    first = session.query(State).first()
+    print(f"{first.id}: {first.name}")
