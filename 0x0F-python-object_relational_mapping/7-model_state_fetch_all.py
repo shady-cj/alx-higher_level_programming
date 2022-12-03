eca5a5687b85f2267ea0bf99dea6441e9690c455
peter <petersp2000@gmail.com>
2022-12-03 19:15:20 +0100

    challenge #7 solution

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,7 +12,7 @@ import sys
 if __name__ == "__main__":
     url = 'mysql+mysqldb://{}:{}@localhost/{}'\
             .format(sys.argv[1], sys.argv[2], sys.argv[3])
-    engine = create_engine(url, pool_pre_ping=True, echo=True)
+    engine = create_engine(url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker()
     session = Session(bind=engine)
