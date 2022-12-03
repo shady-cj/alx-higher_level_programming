e1706c5c383c2b27df8242a03fb39a4dffb436c6
peter <petersp2000@gmail.com>
2022-12-03 02:32:22 +0100

    Challenge #0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-
+"""
+This module uses MySQLdb to connect to a database passed from the command
+line and performs sql operations (CRUD)
+"""
 import MySQLdb
 import sys
 
 def main(username, password, db_name):
+    """
+    The main function acts as the entry point into the main functionality
+    It makes the connection and calls the cursor on the db connection which is
+    later used to make sql queries
+    """
     db = MySQLdb.connect(host='localhost', db=db_name, user=username, passwd=password, port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM STATES")
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     cursor.fetchall()
     cursor.close()
     db.close()
