d8884347c955f36a85392be143095eda763e1842
peter <petersp2000@gmail.com>
2022-12-04 00:24:56 +0100

    challenge #4 solution

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,15 +14,15 @@ from model_state import State, Base
 if __name__ == "__main__":
     url = "mysql+mysqldb://{}:{}@localhost:3306/{}"\
         .format(sys.argv[1], sys.argv[2], sys.argv[3])
-    engine = create_engine(url, pool_pre_ping=True, echo=True)
+    engine = create_engine(url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    # q = session.query(State.name, City.id, City.name)\
-    #     .filter(City.state_id == State.id)\
-    #     .order_by(City.id).all()
-    q = session.query(State, City).join(State).order_by(City.id).all()
-    for state, city in q:
-        print(f"{state.name}: ({city.id}) {city.name}")
-    # for state_name, city_id, city_name in q:
-    #     print(f"{state_name}: ({city_id}) {city_name}")
+    q = session.query(State.name, City.id, City.name)\
+        .filter(City.state_id == State.id)\
+        .order_by(City.id).all()
+    # q = session.query(State, City).join(State).order_by(City.id).all()
+    # for state, city in q:
+    #     print(f"{state.name}: ({city.id}) {city.name}")
+    for state_name, city_id, city_name in q:
+        print(f"{state_name}: ({city_id}) {city_name}")
