518fb42e09d6bd69123a304f747e43c86f072029
peter <petersp2000@gmail.com>
2022-12-04 01:50:14 +0100

    challenge #15 solution

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Using SQLAlchemy to define a database schema. for City.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """
+    Defining the database schema
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship('State', back_populates="cities")
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Using SQLAlchemy to define a database schema.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Defining the database schema
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade = "all, delete-orphan", back_populates="state")
+
