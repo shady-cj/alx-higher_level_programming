1a3ec5a90c24bedcc9c2f7cffba62157524590c1
shady-cj <petersp2000@gmail.com>
2022-12-16 05:07:38 +0100

    update task 2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # use curl to display content
-curl -s "$1"
+curl -sL "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # display methods accepted by a server
-curl -s -X "OPTIONS" "$1" | grep "Allow" | cut -d ':' -f 2 | sed 's/ //'
+curl -I "$1" 
+
+#| grep "Allow" | cut -d ':' -f 2 | sed 's/ //'
