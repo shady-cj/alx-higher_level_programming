5d86ac8f9f73b6b531cd5681f7322c77b244755b
shady-cj <petersp2000@gmail.com>
2022-12-18 14:22:13 +0100

    linting 6-peak.py so it passes pycodestyle tests

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,31 +4,32 @@ Contains a function thats finds a peak in a
 list of unsorted integers
 """
 
+
 def findPeakRecursive(nums, lo, hi):
     """
     Using recursion to effectively search
     for the peak element using the divide and
     conquer algorithm
     """
-    mid = (lo + hi) // 2       
+    mid = (lo + hi) // 2
     left = mid - 1
     right = mid + 1
     if left < 0 and nums[mid] >= nums[right]:
-            return mid
+        return mid
     elif right >= len(nums) and nums[mid] >= nums[left]:
-            return mid
+        return mid
     elif nums[mid] >= nums[left] and nums[mid] >= nums[right]:
         return mid
 
     if lo == hi:
         return None
-    
     searchLeft = findPeakRecursive(nums, lo, mid)
     searchRight = findPeakRecursive(nums, mid + 1, hi)
     if searchLeft is None:
         return searchRight
     else:
         return searchLeft
+
 
 def find_peak(list_of_integers):
     """
