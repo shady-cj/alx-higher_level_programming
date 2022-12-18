8d486d900400846d6e1d842acd5ab1d5c2dbaace
shady-cj <petersp2000@gmail.com>
2022-12-18 14:12:45 +0100

    Implementing peak element solution

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,31 @@ Contains a function thats finds a peak in a
 list of unsorted integers
 """
 
+def findPeakRecursive(nums, lo, hi):
+    """
+    Using recursion to effectively search
+    for the peak element using the divide and
+    conquer algorithm
+    """
+    mid = (lo + hi) // 2       
+    left = mid - 1
+    right = mid + 1
+    if left < 0 and nums[mid] >= nums[right]:
+            return mid
+    elif right >= len(nums) and nums[mid] >= nums[left]:
+            return mid
+    elif nums[mid] >= nums[left] and nums[mid] >= nums[right]:
+        return mid
+
+    if lo == hi:
+        return None
+    
+    searchLeft = findPeakRecursive(nums, lo, mid)
+    searchRight = findPeakRecursive(nums, mid + 1, hi)
+    if searchLeft is None:
+        return searchRight
+    else:
+        return searchLeft
 
 def find_peak(list_of_integers):
     """
@@ -13,13 +38,7 @@ def find_peak(list_of_integers):
         return None
     if len(list_of_integers) == 1:
         return list_of_integers[0]
-    mid = len(list_of_integers) // 2
-    left = find_peak(list_of_integers[0:mid])
-    right = find_peak(list_of_integers[mid:])
-    if right is not None and left is not None:
-        if right >= left:
-            return right
-        else:
-            return left
-    else:
-        return right or left
+    index = findPeakRecursive(list_of_integers, 0, len(list_of_integers) - 1)
+    if index is not None:
+        return list_of_integers[index]
+    return None
