742ef5544f8a6ee3adc234709a890198e3f47376
shady-cj <petersp2000@gmail.com>
2023-01-18 17:50:55 +0100

    storing a web page into a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/**
+ * a script that gets the content of a web page
+ * and stores it in a file
+ * arg1: url to scrape
+ * arg2: filepath
+ */
+
+if (process.argv.length < 4) process.exit();
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (error, res, body) {
+  if (error) { console.log(error); } else {
+   fs.writeFile(process.argv[3], body, {encoding: 'utf8'}, (err) => {
+      if (err)
+         console.error(err);
+   })
+  }
+});
