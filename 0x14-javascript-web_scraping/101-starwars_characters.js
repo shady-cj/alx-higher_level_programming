065ff69d37c11f965c3401d6931bc46f6f53ef59
shady-cj <petersp2000@gmail.com>
2023-01-18 20:01:13 +0100

    movie characters in order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/**
+ * a script that prints all characters of a star wars movie
+ * in the order they appear from the api
+ * Usage: ./100-starwars_characters id
+ */
+
+if (process.argv.length < 3) process.exit();
+const request = require('request');
+
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, res, body) {
+  if (error) { console.log(error); } else {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+    const fetchCharacters = async () => {
+      for (const characterUrl of characters) {
+        const promise = new Promise((resolve, reject) => {
+            request(characterUrl, (error, res, body) => {
+            if (error) {
+              reject(error)
+            } else {
+              const karacter = JSON.parse(body);
+              resolve(karacter.name);
+	   } 
+           });
+	});
+        const response = await promise;
+        console.log(response);
+      }
+    }
+    fetchCharacters();
+  }
+});
--- 0x14-javascript-web_scraping/README.md
+++ 0x14-javascript-web_scraping/README.md
@@ -212,3 +212,44 @@ C-3PO
 Lando Calrissian
 guillaume@ubuntu:~/0x14$ 
 ```
+
+
+
+
+## 8. Right order
+Write a script that prints all characters of a Star Wars movie:
+
+* The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+* Display one character name by line in the same order of the list “characters” in the /films/ response
+* You must use the Star wars API
+* You must use the module request
+
+
+
+**File** - 101-starwars_characters.js
+
+
+```
+guillaume@ubuntu:~/0x14$ ./101-starwars_characters.js 3
+Luke Skywalker
+C-3PO
+R2-D2
+Darth Vader
+Leia Organa
+Obi-Wan Kenobi
+Chewbacca
+Han Solo
+Jabba Desilijic Tiure
+Wedge Antilles
+Yoda
+Palpatine
+Boba Fett
+Lando Calrissian
+Ackbar
+Mon Mothma
+Arvel Crynyd
+Wicket Systri Warrick
+Nien Nunb
+Bib Fortuna
+guillaume@ubuntu:~/0x14$ 
+```
